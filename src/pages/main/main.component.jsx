2309ca31e5b28e45845572fdc6d83361288babd9
Alan Tran <contactalantran@gmail.com>
2021-05-19 10:48:09 -0400

    cleaned up code a bit

--- src/pages/main/main.component.jsx
+++ src/pages/main/main.component.jsx
@@ -2,8 +2,6 @@ import React, { useContext } from 'react';
 import IntroCard from '../../components/intro-card/intro-card.component';
 import MessageCard from '../../components/message-card/message-card.component';
 import OurStoryCard from '../../components/our-story-card/our-story-card.component';
-import SignInOrSignUpCard from '../../components/sign-in-or-sign-up-card/sign-in-or-sign-up-card.component';
-import RsvpCard from '../../components/rsvp/rsvp.component';
 
 import { CurrentUserContext } from '../../providers/user/user.provider';
 
